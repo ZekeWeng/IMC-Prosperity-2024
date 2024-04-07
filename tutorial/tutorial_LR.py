f871e6cb0c814e842de5c1b2d0a461cf51ca5bb0
WhxyzTrees <ZekeWengP@gmail.com>
2024-04-06 20:53:17 -0400

    Update tutorial_LR.py

--- tutorial/tutorial_LR.py
+++ tutorial/tutorial_LR.py
@@ -1,8 +1,6 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
-
-
 import numpy as np
 import statistics
 import math
@@ -10,63 +8,92 @@ import math
 class Trader:
 
     PRODUCTS = {
-        'STARFRUIT': {"TIME": [], "DATA": [], "PRICE": 0, "LR_SIZE": 0, "PLIMIT": 0},
-        'AMETHYSTS': {"TIME": [], "DATA": [], "PRICE": 0, "LR_SIZE": 0, "PLIMIT": 0}
+        'STARFRUIT': {"QUANTITY": 0, "TIME": [], "DATA": [], "PRICE": 0, "LR_SIZE": 10, "PLIMIT": 20, "PNL": 0},
+        'AMETHYSTS': {"QUANTITY": 0, "TIME": [], "DATA": [], "PRICE": 0, "LR_SIZE": 10, "PLIMIT": 20, "PNL": 0}
         }
 
     def run(self, state: TradingState):
 
-        POSITION_LIMITS = {'STARFRUIT': 20, 'AMETHYSTS': 20}
-
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
+
+        time = state.timestamp
 
         result = {}
         for product in state.order_depths:
 
+            ### Game Function
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
 
-            regression_size = 50
+            ### Compute Regressions when sufficient data points
+            if len(Trader.PRODUCTS[product]["TIME"]) > Trader.PRODUCTS[product]["LR_SIZE"]:
+                Trader.PRODUCTS[product]["PRICE"] = Trader.compute_price_regression(product, time)
 
-            if len(Trader.PRODUCTS[product]["TIME"]) > regression_size:
-                if Trader.PRODUCTS[product]["TIME"][-1] % (regression_size*10) == 0:
-                    X = np.array(Trader.PRODUCTS[product]["TIME"][-regression_size:])
-                    y = np.array(Trader.PRODUCTS[product]["DATA"][-regression_size:])
-
-                    X = X[:, np.newaxis]
-                    X = np.hstack([np.ones_like(X), X])
-
-                    coefficients, residuals, rank, s = np.linalg.lstsq(X, y, rcond=None)
-                    intercept, slope = coefficients
-
-                    Trader.PRODUCTS[product]["PRICE"] = state.timestamp * slope + intercept
-
+            ### Store prices + quantities
             if len(order_depth.buy_orders) != 0 and len(order_depth.sell_orders) != 0:
-                Trader.PRODUCTS[product]["DATA"].append(statistics.mean([list(order_depth.buy_orders.items())[0][0], list(order_depth.sell_orders.items())[0][0]]))
-                Trader.PRODUCTS[product]["TIME"].append(state.timestamp)
+                Trader.PRODUCTS[product]["DATA"].append(statistics.mean([Trader.weighted_average_price(order_depth.buy_orders), Trader.weighted_average_price(order_depth.sell_orders)]))
+                Trader.PRODUCTS[product]["TIME"].append(time)
+
+            print("Median price: " + str(statistics.mean([list(order_depth.buy_orders.items())[0][0], list(order_depth.sell_orders.items())[0][0]])))
+            print("WA price: " + str(Trader.weighted_average_price(order_depth.buy_orders)))
 
             print("Acceptable price : " + str(Trader.PRODUCTS[product]["PRICE"]))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
 
+            ### BUY ORDERS
             if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < Trader.PRODUCTS[product]["PRICE"]:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
+                ask, ask_amount = list(order_depth.sell_orders.items())[0]
+                if int(ask) < Trader.PRODUCTS[product]["PRICE"] and time > Trader.PRODUCTS[product]["LR_SIZE"] * 100:
+                    order = Trader.submit_order(product, "BUY", ask, ask_amount)
+                    orders.append(order)
 
+            ### SELL ORDERS
             if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > Trader.PRODUCTS[product]["PRICE"]:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                bid, bid_amount = list(order_depth.buy_orders.items())[0]
+                if int(bid) > Trader.PRODUCTS[product]["PRICE"] and time > Trader.PRODUCTS[product]["LR_SIZE"] * 100:
+                    order = Trader.submit_order(product, "SELL", bid, bid_amount)
+                    orders.append(order)
 
             result[product] = orders
 
         # String value holding Trader state data required.
         # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE"
+        traderData = "SAMPLE" # ID TEAM???
 
         # Sample conversion request. Check more details below.
+        # FIGURE THIS OUT LATER
         conversions = 1
         return result, conversions, traderData
+
+    def submit_order(product, action, price, quantity):
+        order_quantity = min(abs(quantity), Trader.PRODUCTS[product]["PLIMIT"])
+        print(action, str(order_quantity) + "x", price)
+        if action == "BUY":
+            return Order(product, price, order_quantity)
+        if action == "SELL":
+            return Order(product, price, -order_quantity)
+
+    def compute_price_regression(product, time):
+        X = np.array(Trader.PRODUCTS[product]["TIME"][-Trader.PRODUCTS[product]["LR_SIZE"]:])
+        y = np.array(Trader.PRODUCTS[product]["DATA"][-Trader.PRODUCTS[product]["LR_SIZE"]:])
+
+        X = X[:, np.newaxis]
+        X = np.hstack([np.ones_like(X), X])
+
+        coefficients, _residuals, _rank, _s = np.linalg.lstsq(X, y, rcond=None)
+        intercept, slope = coefficients
+
+        return time * slope + intercept
+
+    def compute_momentum():
+        pass
+
+    def weighted_average_price(price_quantity_dict):
+        total_quantity = sum(price_quantity_dict.values())
+        total_value = sum(price * quantity for price, quantity in price_quantity_dict.items())
+
+        if total_quantity == 0:
+            return 0
+
+        return total_value / total_quantity
